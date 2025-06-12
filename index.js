8f821d1c67f12874a41b61c6f17867f10d47f694
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-06-12 18:38:38 -0500

    new stuff

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("happy thursday")
+// const { format, addDays } = require('./node_modules/date-fns/index.js')
+
+import { format, addDays } from 'date-fns';
+
+const today = new Date();
+const formattedDate = format(today, 'MMMM do, yyyy');
+const nextWeek = format(addDays(today, 7), 'MMMM do, yyyy');
+
+// document.getElementById('formatted-date').innerHTML = `
+//     Today: ${formattedDate}<br>
+//     Next week: ${nextWeek}
+// `;
+
+console.log(today)
+console.log(`Formatted date: ${formattedDate}`);
+console.log(`Next week: ${nextWeek}`);
